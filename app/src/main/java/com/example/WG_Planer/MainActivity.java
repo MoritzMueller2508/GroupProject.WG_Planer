37018d9652e98c5d1d919cf05c736c34c867a2df
Zusibell.Jimenez <zusibell@gmail.com>
2020-04-08 14:55:50 +0200

    name from the project changed

--- app/src/main/java/com/example/WG_Planer/MainActivity.java
+++ app/src/main/java/com/example/WG_Planer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.WG_Planer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/WG_Planer/ExampleUnitTest.java
+++ app/src/test/java/com/example/WG_Planer/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.WG_Planer;
 
 import org.junit.Test;
 
