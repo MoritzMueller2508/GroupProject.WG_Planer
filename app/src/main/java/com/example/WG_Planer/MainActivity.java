62b8ce8bc482f5042dd529e87ea1415c3f59de15
Zusibell.Jimenez <zusibell@gmail.com>
2020-04-11 00:49:17 +0200

    aws amplify configured, project initialized, api and database created, cognito user auth installed

--- app/src/main/java/com/example/WG_Planer/MainActivity.java
+++ app/src/main/java/com/example/WG_Planer/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.welcome_start);
+        setContentView(R.layout.log_in);
     }
 }
