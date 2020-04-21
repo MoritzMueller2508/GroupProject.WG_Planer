037da43a048b3508ec9f8ded84cda155deb07bc6
Zusibell.Jimenez <zusibell@gmail.com>
2020-04-21 17:41:57 +0200

    Moritzt hat alles kaputt gemacht

--- app/src/main/java/com/example/wgplaner/MainActivity.java
+++ app/src/main/java/com/example/wgplaner/MainActivity.java
@@ -1,23 +1,22 @@
 package com.example.wgplaner;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
